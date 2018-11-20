a25524294dfdde8a220def728e567ce48b22f417
Tobias Jahnke <tobias.jahnke@microchip.com>
2018-11-20 15:46:42 +0100

    hal-unicens: switched to cmaketemplate
    Bug-AGL: SPEC-1505
    
    Signed-off-by: Tobias Jahnke <tobias.jahnke@microchip.com>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -29,9 +29,9 @@ set(PROJECT_AUTHOR_MAIL "jonathan.aillet@iot.bzh")
 set(PROJECT_LICENSE "APL2.0")
 set(PROJECT_LANGUAGES "C")
 
-# Where are stored default templates files from submodule or subtree app-templates in your project tree
+# Where are stored the project configuration files
 # relative to the root project directory
-set(PROJECT_APP_TEMPLATES_DIR "conf.d/app-templates")
+set(PROJECT_CMAKE_CONF_DIR "conf.d")
 
 # Where are stored your external libraries for your project. This is 3rd party library that you don't maintain
 # but used and must be built and linked.
@@ -209,5 +209,8 @@ set(PACKAGE_MESSAGE "Install widget file using in the target : afm-util install 
 
 # This include is mandatory and MUST happens at the end
 # of this file, else you expose you to unexpected behavior
+#
+# This CMake module could be found at the following url:
+# https://gerrit.automotivelinux.org/gerrit/#/admin/projects/src/cmake-apps-module
 # -----------------------------------------------------------
-include(${PROJECT_APP_TEMPLATES_DIR}/cmake/common.cmake)
+include(CMakeAfbTemplates)
